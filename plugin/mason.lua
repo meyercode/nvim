b05999ff20da40c171e552c7a1304d172891f9b7
meyercode <adam@meyerfam.com>
2022-10-25 21:08:34 +0200

    Update rust-tools

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -43,31 +43,10 @@ require "mason-lspconfig".setup_handlers {
             server = {
                 on_attach = function(_, bufnr)
                     -- Hover actions
-                    vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+                    vim.keymap.set("n", "<leader>h", rt.hover_actions.hover_actions, { buffer = bufnr })
                     -- Code action groups
-                    vim.keymap.set("n", "<Leader>c", rt.code_action_group.code_action_group, { buffer = bufnr })
+                    vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
                 end,
-                settings = {
-                    ["rust-analyzer"] = {
-                        imports = {
-                            granularity = {
-                                group = "module",
-                            },
-                            prefix = "self",
-                        },
-                        cargo = {
-                            buildScripts = {
-                                enable = true,
-                            },
-                        },
-                        checkOnSave = {
-                            command = "clippy"
-                        },
-                        procMacro = {
-                            enable = true
-                        },
-                    }
-                }
             }
         }
         rt.setup(opts)
