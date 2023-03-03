795326e582093d6785dc98cf57837f1118d28e43
meyercode <adam@meyerfam.com>
2023-03-03 12:12:59 +0100

    Add find all and grep all to telescope while still ignoring node_modules and .git

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,12 +9,23 @@ require("telescope").setup {
                 ["C-j"] = actions.move_selection_next, -- TODO: not working for some reason...
                 ["C-k"] = actions.move_selection_previous,
             }
-        }
+        },
+        file_ignore_patterns = { "node_modules/", ".git/" },
     }
 }
 
 map.set("n", "<Leader>ff", builtin.find_files, {})
+map.set("n", "<Leader>faf", function()
+    builtin.find_files({ no_ignore = true, hidden = true })
+end)
 map.set("n", "<Leader>fg", builtin.live_grep, {})
+map.set("n", "<Leader>fag", function()
+    builtin.live_grep({
+        additional_args = function(opts)
+            return { "--hidden", "--no-ignore", }
+        end
+    })
+end)
 map.set("n", "<leader>fr", function()
     builtin.oldfiles({ only_cwd = true })
 end)
