1d09e77260be6813d24a3549fd8cbfffb31c6bbd
meyercode <adam@meyerfam.com>
2024-08-30 23:01:14 +0200

    fix gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup {}
+	end,
 	keys = {
 		{ "<leader>gb", function() require("gitsigns").toggle_current_line_blame() end },
+		{ "<leader>gB", function() require("gitsigns").blame() end },
 		{ "<leader>ga", function() require("gitsigns").stage_hunk() end },
 		{ "<leader>gu", function() require("gitsigns").undo_stage_hunk() end },
 		{ "<leader>gr", function() require("gitsigns").reset_hunk() end },
 		{ "<leader>gp", function() require("gitsigns").preview_hunk() end },
-		{ "<leader>gb", function() require("gitsigns").blame_line { full = true } end },
 		{ "<leader>gd", function() require("gitsigns").diffthis() end },
 		{ "<leader>gD", function() require("gitsigns").diffthis("~") end },
 
@@ -24,4 +27,5 @@ return {
 			end
 		},
 	},
+	lazy = false,
 }
